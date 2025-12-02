df3cf1309dce42160b0c60840317908a28f56ad9
Dharmil <dharmil132@gmail.com>
2025-12-02 18:06:26 +0530

    Next Permutation

---
+++ Array/Problems/leetcode/Medium/Permutation.java
@@ -0,0 +1,80 @@
+package Array.Problems.leetcode.Medium;
+
+import java.util.Arrays;
+
+/*
+ * 
+ * 
+ * Leetcode - # 31 - Next Permutation
+ * 
+ * 
+ */
+
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+
+// For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+// For example, the next permutation of arr = [1,2,3] is [1,3,2].
+// Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+// While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+// Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
+
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+public class Permutation {
+     public static void main(String[] args) {
+          int arr[] = new int[] { 2, 1, 5, 4, 3, 0, 0 };
+          // int arr[] = new int[] { 4, 3, 2, 1 };
+
+          optimal(arr);
+     }
+
+
+     /*
+          TC - O(N)
+          SC - O(1)
+      */
+     public static void optimal(int arr[]) {
+          int part = -1;
+
+          for (int i = arr.length - 2; i >= 0; i--) {
+               if (arr[i] < arr[i + 1]) {
+                    part = i;
+                    break;
+               }
+          }
+          if (part == -1) {
+               int i = 0;
+               int n = arr.length;
+               while (i < n / 2) {
+                    int temp = arr[i];
+                    arr[i] = arr[n - i - 1];
+                    arr[n - i - 1] = temp;
+                    i++;
+               }
+               System.out.println(Arrays.toString(arr));
+               return;
+          }
+          for (int i = arr.length - 1; i >= part; i--) {
+               if (arr[i] > arr[part]) {
+                    int temp = arr[i];
+                    arr[i] = arr[part];
+                    arr[part] = temp;
+                    break;
+               }
+          }
+          int left = part + 1;
+          int right = arr.length-1;
+          while (left < right) {
+               int temp = arr[left];
+               arr[left] = arr[right];
+               arr[right] = temp;
+               left++;
+               right--;
+          }
+          System.out.println(Arrays.toString(arr));
+     }
+}
