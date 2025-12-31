0cca1d4ce18418cd4e8d0a0c1a70d2d536e2ab24
Dharmil <dharmil132@gmail.com>
2025-12-31 18:16:25 +0530

    Search in rotated array

--- Binary_Search/Problems/Leetcode/Medium/Search_Rotated_Array.java
+++ Binary_Search/Problems/Leetcode/Medium/Search_Rotated_Array.java
@@ -2,37 +2,35 @@ package Binary_Search.Problems.Leetcode.Medium;
 
 public class Search_Rotated_Array {
      public static void main(String[] args) {
-          int arr[] = new int[]{4,5,6,7,0,1,2};
-          int target = 7;
+          int arr[] = new int[] { 4, 5, 6, 7, 0, 1, 2 };
+          int target = 1;
 
           System.out.println(solution(arr, target));
      }
 
-
-     public static int solution(int arr[], int target){
+     public static int solution(int arr[], int target) {
           int low = 0;
-          int high = arr.length-1;
-          int ans = -1;
-          while(low<=high){
-               int mid = (low+high)/2;
-               if(arr[mid] == target){
-                    return mid;
-               }
-               if(arr[mid] > target){
-                    // reverse the direction 
-                    if(arr[low] > target){
-                         low = mid+1;
-                    } else{
-                         high = mid-1;
-                    }
-               } else{
-                    if(arr[mid] > target){
+          int high = arr.length - 1;
+          while (low <= high) {
+               int mid = (low + high) / 2;
+
+               if(arr[mid] == target) return mid;
+
+               if (arr[low] <= arr[mid]) {
+                    if (arr[low] <= target && target < arr[mid]) {
                          high = mid - 1;
-                    } else{
-                         high = mid-1;
+                    } else {
+                         low = mid + 1;
+                    }
+               } else {
+                    if (arr[mid] < target && target <= arr[high]) {
+                         low = mid + 1;
+                    } else {
+                         high = mid - 1;
                     }
                }
           }
+
           return -1;
      }
 }
